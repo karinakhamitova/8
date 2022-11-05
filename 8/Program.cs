5b43b8c17c041977db75885b4f5815690d6d51b3
TopFps <TopFps@DESKTOP-UUEU6H0>
2022-11-06 01:34:12 +0300

    Добавьте файлы проекта.

---
+++ 8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\С#";
+            string[] files = Directory.GetFiles(path,".",SearchOption.AllDirectories);
+            foreach (string file in files)
+            {
+                Console.WriteLine(file);
+            }
+              
+            Console.ReadKey();
+        }
+    }
+}
